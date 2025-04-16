fc2d742cc5296c658f5560b36b0c816c1d646451
Ashim <ashim.adhikari86@gmail.com>
2025-04-16 14:03:13 +0800

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/hello/')
+      .then(res => res.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
